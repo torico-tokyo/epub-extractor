f39041038d9cea4833476b92a41020f3a89a5599
ytyng <ytyng@live.jp>
2024-09-03 11:05:27 +0900

    Setup long description.

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+import os
+
 from setuptools import setup
-from epub_extractor import __author__, __version__, __license__
+
+from epub_extractor import __author__, __license__, __version__
+
+
+def get_long_description():
+    return open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
 
 setup(
     name='epub-extractor',
     version=__version__,
-    description='Extract comic EPUB pages to Jpeg files, '
-                'Dump meta information.',
+    description=(
+        'Extract comic EPUB pages to Jpeg files, Dump meta information.'
+    ),
+    long_description=get_long_description(),
     license=__license__,
     author=__author__,
     author_email='ytyng@live.jp',
